d64505cc861e6f5cba8d9a3557cc959fd181eef8
Michael Pernpeintner <michaelpernpeintner@gmail.com>
2020-12-07 17:36:04 +0100

    Added Test.vue

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about">
     <h1>This is a test page for API calls</h1>
+    <button @click="callApi">
+      Call API
+    </button>
+    <router-link to="/">Back to home</router-link>
   </div>
-  <button @click="callApi">
-    Call API
-  </button>
-  <router-link to="/">Back to home</router-link>
 </template>
 
 <script>
