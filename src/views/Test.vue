c906ea8aa92dc229e0bb2debdbf23a76303e5ebd
Michael Pernpeintner <michaelpernpeintner@gmail.com>
2020-12-07 17:23:57 +0100

    Added Test.vue

---
+++ src/views/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is a test page for API calls</h1>
+  </div>
+  <button @click="callApi">
+    Call API
+  </button>
+</template>
+
+<script>
+  function callApi() {
+    alert("API called!")
+  }
+</script>
